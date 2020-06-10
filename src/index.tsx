65f04554690465988c2c09eecf820fb6d1890157
yaakaito <yaakaito@gmail.com>
2020-06-11 04:11:05 +0900

    style: lint

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,7 @@ import styles from './index.css'
 
 ReactDOM.render(
     <div>
-        <h1
-            className={styles.title}
-        >Hello</h1>
+        <h1 className={styles.title}>Hello</h1>
     </div>,
-document.querySelector('#root'))
+    document.querySelector('#root')
+)
